1746d0624174b6ba0896dbf1d2f0a53954b80af2
Giovanni Moscato <69764323+codingcodewhilegoofin@users.noreply.github.com>
2020-12-22 17:10:55 -0600

    Update2
    Update2

--- RISCVMemorysim/memory.cpp
+++ RISCVMemorysim/memory.cpp
@@ -1,15 +1,4 @@
- //***********************************************************************************
- //*
- //* 	memory.cpp
- //*	CSCI 463 Assignment 3 
- //*
- //* 	Giovanni Moscato Z1820207
- //*
- //*	Memory Simulator
- //* 	Instructor: John Winans
- //* 	Section: -0001
- //*
- //***********************************************************************************
+
 
 // Local header file 
 #include "memory.h"
